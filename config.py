b779ae9d408184364572754f61c796486e5ac9f8
milk <492627639@qq.com>
2024-06-24 22:06:25 +0800

    定义好各个模块

---
+++ config.py
@@ -0,0 +1,1 @@
+BERT_MODEL = "./hgmodel/bert-base-chinese/"
--- model.py
+++ model.py
@@ -1,3 +1,27 @@
 """
-分类模型文件
+基于bert实现 实现 textcnn
 """
+
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+import torch.nn as nn
+import torch
+from config import *
+
+class TextCNN(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        
+        self.bert = AutoModelForMaskedLM(BERT_MODEL)
+        # bert不参与参数更新
+        for name, param in self.bert.name_parameters():
+            param.requires_grad = False
+        self.convs = nn.ModuleList([nn.Conv2d(1, NUM_FILTERS, (i, EMBEDDING_DIM))for i in FILTER_SIZES])
+        self.linear = nn.Linear(NUM_FILTERS * 3, NUM_CLASSES)
+        
+
+
+if __name__== "__main__":
+    
+    pass
+
+
